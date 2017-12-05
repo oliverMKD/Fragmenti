60db738b041f6699547018449606f8cb633bdfbb
oliverMKD <oliverdamjan2013@gmail.com>
2017-12-05 18:54:11 +0100

    4 izmena

--- app/src/main/java/com/oli/fragmenti/MainActivity.java
+++ app/src/main/java/com/oli/fragmenti/MainActivity.java
@@ -1,13 +1,30 @@
 package com.oli.fragmenti;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.pager)
+    ViewPager pager;
+    @BindView(R.id.tabLayout)
+    TabLayout tablayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setUpViewPager(pager);
+        tablayout.setupWithViewPager(pager);
+    }
+
+    public void setUpViewPager(ViewPager upViewPager) {
+        myPageAdapter adapter = new myPageAdapter(this.getSupportFragmentManager());
+        pager.setAdapter(adapter);
     }
 }
