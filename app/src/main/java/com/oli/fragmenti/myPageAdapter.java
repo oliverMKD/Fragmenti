8d7286e6a7c1aba415b745a4890a7ff70fb2cf51
oliverMKD <oliverdamjan2013@gmail.com>
2017-12-05 18:40:28 +0100

    treta izmena

---
+++ app/src/main/java/com/oli/fragmenti/myPageAdapter.java
@@ -0,0 +1,41 @@
+package com.oli.fragmenti;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by oliver on 12/5/2017.
+ */
+
+public class myPageAdapter extends FragmentPagerAdapter {
+
+    ArrayList<Fragment> fragments = new ArrayList<>();
+    ArrayList<String> titles = new ArrayList<>();
+
+    public void addFragment(Fragment fragment, String title){
+        titles.add(title);
+        fragments.add(fragment);
+    }
+
+    public myPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles.get(position);
+    }
+}
